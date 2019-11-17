4a4701889c974fe4db3c54a34c7643525cf66ba4
赵凡 <zhaofan.qa@bytedance.com>
2019-11-17 21:21:12 +0800

    加入数据库及表的各项配置

---
+++ app/src/main/java/com/coolweather/android/db/County.java
@@ -0,0 +1,14 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+public class County extends DataSupport {
+
+    private int id;
+
+    private String countyName;
+
+    private String weatherId;
+
+    private int cityId;
+}
